275fe5f1b134b18c4e00b161b2999f6551810ea7
иван рыбников <rybnikovv12345@gmail.com>
2021-04-24 12:29:16 +0300

    [+] 2.1*

---
+++ API/SimbirSoft.Intensive.BL/PeopleGetBook/Models/PeopleGetBook.cs
@@ -0,0 +1,33 @@
+﻿using SimbirSoft.Intensive.BL.Books.Models;
+using SimbirSoft.Intensive.BL.Peoples.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SimbirSoft.Intensive.BL.PeopleGetBook.Models
+{
+    /// <summary>
+    /// Класс, отвечающий за получение человеком книги
+    /// </summary>
+    public class PeopleGetBook
+    {
+        /// <summary>
+        /// Человек
+        /// </summary>
+        public PeopleDto People { get; set; }
+
+        /// <summary>
+        /// Книга
+        /// </summary>
+        public BookDto Book { get; set; }
+
+        /// <summary>
+        /// Дата и время получения книги
+        /// </summary>
+        public DateTimeOffset DateAndTime { get; set; }
+
+        /// <summary>
+        /// Список, отвечающий за хранение сущностей
+        /// </summary>
+        public static List<PeopleGetBook> peopleGetBooks = new List<PeopleGetBook>();
+    }
+}
